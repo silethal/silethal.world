2cbe6bab8c96f56b2ee1f9f0910216cf93574172
Ben Weedon <ben.weedon@outlook.com>
2017-08-23 00:29:38 -0700

    Support a very basic form of pages
    Pages now have their own layout which extends the base layout.

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-var Metalsmith  = require('metalsmith');
-var markdown    = require('metalsmith-markdown');
-var layouts     = require('metalsmith-layouts');
-var permalinks  = require('metalsmith-permalinks');
+var Metalsmith        = require('metalsmith');
+var markdown          = require('metalsmith-markdown');
+var layouts           = require('metalsmith-layouts');
+var permalinks        = require('metalsmith-permalinks');
+var pageData          = require('metalsmith-page-data');
+
+var handlebars        = require('handlebars');
+var handlebarsLayouts = require('handlebars-layouts');
+
+// handlebars config
+handlebars.registerHelper(handlebarsLayouts(handlebars));
 
 Metalsmith(__dirname)
     .metadata({
@@ -10,11 +17,21 @@ Metalsmith(__dirname)
         generator: 'Metalsmith',
         url: 'http://www.metalsmith.io/'
     })
+    .use(pageData([
+        {
+            pattern: 'pages/**/*.md',
+            data: {
+                layout: 'page.html'
+            }
+        }
+    ]))
     .use(markdown())
     .use(permalinks())
     .use(layouts({
-        engine: 'handlebars'
+        engine: 'handlebars',
+        partials: 'layouts'
     }))
     .build(function(err, files) {
         if (err) { throw err; }
-    });
+    }
+);
---
+++ src/pages/world/silethal.md
@@ -0,0 +1,5 @@
+---
+title: Silethal
+---
+
+Hi there
