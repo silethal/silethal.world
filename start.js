8e9d578667914f00647ebf8bfdbc5451dfdf7381
Ben Weedon <ben.weedon@outlook.com>
2017-08-26 12:48:20 -0700

    Turn off clean builds when using metalsmith-start

--- start.js
+++ start.js
@@ -1,5 +1,6 @@
 var Runner = require('metalsmith-start').Runner;
 var ms = require('./index.js');
+ms.clean(false); // cleaning during continous build on Windows can cause problems
 
 var r = new Runner(ms);
 r.start(function(){});
