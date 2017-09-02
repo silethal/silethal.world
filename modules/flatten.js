0ac44708c5970e4b54130114465fbb6dfe74bcda
Ben Weedon <ben.weedon@outlook.com>
2017-09-01 23:25:32 -0700

    Fix bug where pages directly under /pages are overwritten in flatten.js
    This was noticed for the session-list page.

--- modules/flatten.js
+++ modules/flatten.js
@@ -8,9 +8,10 @@ function flatten(opts) {
         Object.keys(files).forEach(function(path) {
             // move the file under opts.destination if it matches opts.pattern
             if (multimatch(path, opts.pattern).length > 0) {
+                var file = files[path];
                 var newPath = pathMod.join(opts.destination, pathMod.basename(path));
-                files[newPath] = files[path];
                 delete files[path];
+                files[newPath] = file;
             }
         });
         done();
